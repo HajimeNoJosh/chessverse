4e8263bd5bf5fcac4d91ddf256d1f0ffb3234504
Joshuah Martin <joshuahmartin@Joshuahs-MacBook-Pro.local>
2020-06-25 12:08:02 -0400

    Rather large commit that prevents pieces from moving to illegal squares
    The general premise is that I am filtering out any squares that should not have made it in. If I run into a piece of the same color, stop the loop. If I run into a piece of the opposite color stop the loop. It is a different method for all the pieces, some just required a straight up filter others required working in the loop to stop it directly.
    
    I have also included pawn logic to make the pawn move forward and be able to take as well. Game should be more or less complete other than:
    
    1. Checks
    2. Castling
    3 En passant
    4 Checkmate

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -30,6 +30,13 @@ export const Game = () => {
       setPlayer('white');
     }
   };
+  // This helper function is for finding the opposite color of the current player
+  const whichColor = () => {
+    if (player === 'white') {
+      return 'black';
+    }
+    return 'white';
+  };
 
   const movePiece = (coord, type, color, moved) => {
     const coordFirst = coord[0];
@@ -78,11 +85,17 @@ export const Game = () => {
     setBoardRep(copyBoardRep);
   };
 
-  const continueLoop = (coord) => {
+  const continueLoop = (coord, piece) => {
     const coordFirst = coord[0];
     const coordSecond = coord[1];
-    return boardRep[coordFirst][coordSecond] === '';
+    return (
+      boardRep[coordFirst][coordSecond] === '' ||
+      boardRep[coordFirst][coordSecond].type === piece
+    );
   };
+
+  const filterOutOfBoundary = (coord) =>
+    coord[0] <= 7 && coord[0] >= 0 && coord[1] <= 7 && coord[1] >= 0;
 
   const getPawnMoves = (coord, color, moved) => {
     const tempMoves = [coord];
@@ -107,16 +120,63 @@ export const Game = () => {
         let tempArray = [];
         if (color === 'white') {
           const checkCoord = [coord[0] - i, coord[1]];
+          if (boardRep[checkCoord[0]][checkCoord[1]] !== '') {
+            break;
+          }
           if (continueLoop(checkCoord)) {
             tempArray = [coord[0] - i, coord[1]];
           }
         } else {
           const checkCoord = [coord[0] + i, coord[1]];
+          if (boardRep[checkCoord[0]][checkCoord[1]] !== '') {
+            break;
+          }
           if (continueLoop(checkCoord)) {
             tempArray = [coord[0] + i, coord[1]];
           }
         }
         tempMoves.push(tempArray);
+      }
+    }
+    // For taking with pawns
+    if (color === 'white') {
+      const coordUp = coord[0] - 1;
+      const coordRight = coord[1] + 1;
+      const coordLeft = coord[1] - 1;
+
+      if (
+        coordUp < 8 &&
+        coordUp >= 0 &&
+        coordRight < 8 &&
+        coordRight >= 0 &&
+        coordLeft < 8 &&
+        coordLeft >= 0
+      ) {
+        if (boardRep[coordUp][coordRight].color === 'black') {
+          tempMoves.push([coordUp, coordRight]);
+        }
+        if (boardRep[coordUp][coordLeft].color === 'black') {
+          tempMoves.push([coordUp, coordLeft]);
+        }
+      }
+    } else if (color === 'black') {
+      const coordDown = coord[0] + 1;
+      const coordRight = coord[1] + 1;
+      const coordLeft = coord[1] - 1;
+      if (
+        coordDown < 8 &&
+        coordDown >= 0 &&
+        coordRight < 8 &&
+        coordRight >= 0 &&
+        coordLeft < 8 &&
+        coordLeft >= 0
+      ) {
+        if (boardRep[coordDown][coordRight].color === 'white') {
+          tempMoves.push([coordDown, coordRight]);
+        }
+        if (boardRep[coordDown][coordLeft].color === 'white') {
+          tempMoves.push([coordDown, coordLeft]);
+        }
       }
     }
 
@@ -147,11 +207,17 @@ export const Game = () => {
       ...acc,
       [coord[0] + curr[0], coord[1] + curr[1]],
     ];
-
     const tempMoves = generateNeighbors.reduce(reducer, []);
 
-    setLegalMoves(tempMoves);
-    setActiveLegalMoves(tempMoves, color);
+    const inBoundary = tempMoves.filter((move) => filterOutOfBoundary(move));
+
+    const finalMoves = inBoundary.filter((move) => continueLoop(move, 'King'));
+
+    setLegalMoves(finalMoves);
+    const filteredCoord = finalMoves.filter(
+      (move) => coord.toString() !== move.toString(),
+    );
+    setActiveLegalMoves(filteredCoord, color);
   };
 
   const getKnightMoves = (coord, color) => {
@@ -173,36 +239,71 @@ export const Game = () => {
 
     const tempMoves = generateLegalMoves.reduce(reducer, []);
 
-    setLegalMoves(tempMoves);
-    setActiveLegalMoves(tempMoves, color);
+    const inBoundary = tempMoves.filter((move) => filterOutOfBoundary(move));
+
+    const finalLegalMoves = inBoundary.filter(
+      (move) => boardRep[move[0]][move[1]].color !== player,
+    );
+
+    finalLegalMoves.push(coord);
+
+    setLegalMoves(finalLegalMoves);
+    const filteredCoord = finalLegalMoves.filter(
+      (move) => coord.toString() !== move.toString(),
+    );
+    setActiveLegalMoves(filteredCoord, color);
   };
 
   const getRookMoves = (coord, color) => {
-    const tempMoves = [];
+    const tempMoves = [coord];
     const generateUp = () => {
       const tempUpMoves = [];
-      for (let i = coord[0]; i >= 0; i -= 1) {
+      for (let i = coord[0] - 1; i >= 0; i -= 1) {
+        if (boardRep[i][coord[1]].color === color) {
+          break;
+        } else if (boardRep[i][coord[1]].color === whichColor()) {
+          tempUpMoves.push([i, coord[1]]);
+          break;
+        }
         tempUpMoves.push([i, coord[1]]);
       }
       tempMoves.push(...tempUpMoves);
     };
     const generateDown = () => {
-      const tempUpMoves = [];
-      for (let i = coord[0]; i < 8; i += 1) {
-        tempUpMoves.push([i, coord[1]]);
+      const tempDownMoves = [];
+      for (let i = coord[0] + 1; i < 8; i += 1) {
+        if (boardRep[i][coord[1]].color === color) {
+          break;
+        } else if (boardRep[i][coord[1]].color === whichColor()) {
+          tempDownMoves.push([i, coord[1]]);
+          break;
+        }
+        tempDownMoves.push([i, coord[1]]);
       }
-      tempMoves.push(...tempUpMoves);
+      tempMoves.push(...tempDownMoves);
     };
     const generateRight = () => {
       const tempRightMoves = [];
-      for (let i = coord[1]; i < 8; i += 1) {
+      for (let i = coord[1] + 1; i < 8; i += 1) {
+        if (boardRep[coord[0]][i].color === color) {
+          break;
+        } else if (boardRep[coord[0]][i].color === whichColor()) {
+          tempRightMoves.push([coord[0], i]);
+          break;
+        }
         tempRightMoves.push([coord[0], i]);
       }
       tempMoves.push(...tempRightMoves);
     };
     const generateLeft = () => {
       const tempLeftMoves = [];
-      for (let i = coord[1]; i >= 0; i -= 1) {
+      for (let i = coord[1] - 1; i >= 0; i -= 1) {
+        if (boardRep[coord[0]][i].color === color) {
+          break;
+        } else if (boardRep[coord[0]][i].color === whichColor()) {
+          tempLeftMoves.push([coord[0], i]);
+          break;
+        }
         tempLeftMoves.push([coord[0], i]);
       }
       tempMoves.push(...tempLeftMoves);
@@ -213,46 +314,84 @@ export const Game = () => {
     generateRight();
 
     setLegalMoves(tempMoves);
-    setActiveLegalMoves(tempMoves, color);
+    const filteredCoord = tempMoves.filter(
+      (move) => coord.toString() !== move.toString(),
+    );
+    setActiveLegalMoves(filteredCoord, color);
   };
 
   const getBishopMoves = (coord, color) => {
-    const tempMoves = [];
+    const tempMoves = [coord];
     const generateUpRight = () => {
       const tempUpRightMoves = [];
       let num = 0;
-      for (let i = coord[0]; i >= 0; i -= 1) {
-        tempUpRightMoves.push([i, coord[1] + num]);
+      for (let i = coord[0] - 1; i >= 0; i -= 1) {
         num += 1;
+        const newCoord = coord[1] + num;
+        if (newCoord < 8 && newCoord >= 0) {
+          if (boardRep[i][newCoord].color === color) {
+            break;
+          } else if (boardRep[i][newCoord].color === whichColor()) {
+            tempUpRightMoves.push([i, coord[1] + num]);
+            break;
+          }
+          tempUpRightMoves.push([i, coord[1] + num]);
+        }
       }
       tempMoves.push(...tempUpRightMoves);
     };
     const generateDownRight = () => {
       const tempUpDownMoves = [];
       let num = 0;
-      for (let i = coord[0]; i < 8; i += 1) {
-        tempUpDownMoves.push([i, coord[1] + num]);
+      for (let i = coord[0] + 1; i < 8; i += 1) {
         num += 1;
+        const newCoord = coord[1] + num;
+        if (newCoord < 8 && newCoord >= 0) {
+          if (boardRep[i][newCoord].color === color) {
+            break;
+          } else if (boardRep[i][newCoord].color === whichColor()) {
+            tempUpDownMoves.push([i, coord[1] + num]);
+            break;
+          }
+          tempUpDownMoves.push([i, coord[1] + num]);
+        }
       }
       tempMoves.push(...tempUpDownMoves);
     };
     const generateDownLeft = () => {
       const tempDownLefttMoves = [];
       let num = 0;
-      for (let i = coord[0]; i < 8; i += 1) {
-        tempDownLefttMoves.push([i, coord[1] - num]);
+      for (let i = coord[0] + 1; i < 8; i += 1) {
         num += 1;
+        const newCoord = coord[1] - num;
+        if (newCoord < 8 && newCoord >= 0) {
+          if (boardRep[i][newCoord].color === color) {
+            break;
+          } else if (boardRep[i][newCoord].color === whichColor()) {
+            tempDownLefttMoves.push([i, coord[1] - num]);
+            break;
+          }
+          tempDownLefttMoves.push([i, coord[1] - num]);
+        }
       }
       tempMoves.push(...tempDownLefttMoves);
     };
     const generateUpLeft = () => {
       const tempUpLeftMoves = [];
       let num = 0;
-      for (let i = coord[0]; i >= 0; i -= 1) {
-        tempUpLeftMoves.push([i, coord[1] - num]);
+      for (let i = coord[0] - 1; i >= 0; i -= 1) {
         num += 1;
+        const newCoord = coord[1] - num;
+        if (newCoord < 8 && newCoord >= 0) {
+          if (boardRep[i][newCoord].color === color) {
+            break;
+          } else if (boardRep[i][newCoord].color === whichColor()) {
+            tempUpLeftMoves.push([i, coord[1] - num]);
+            break;
+          }
+          tempUpLeftMoves.push([i, coord[1] - num]);
+        }
       }
-
       tempMoves.push(...tempUpLeftMoves);
     };
     generateUpRight();
@@ -261,72 +400,134 @@ export const Game = () => {
     generateUpLeft();
 
     setLegalMoves(tempMoves);
-    setActiveLegalMoves(tempMoves, color);
+    const filteredCoord = tempMoves.filter(
+      (move) => coord.toString() !== move.toString(),
+    );
+    setActiveLegalMoves(filteredCoord, color);
   };
 
   const getQueenMoves = (coord, color) => {
-    const tempMoves = [];
+    const tempMoves = [coord];
     const generateUpRight = () => {
       const tempUpRightMoves = [];
       let num = 0;
-      for (let i = coord[0]; i >= 0; i -= 1) {
-        tempUpRightMoves.push([i, coord[1] + num]);
+      for (let i = coord[0] - 1; i >= 0; i -= 1) {
         num += 1;
+        const newCoord = coord[1] + num;
+        if (newCoord < 8 && newCoord >= 0) {
+          if (boardRep[i][newCoord].color === color) {
+            break;
+          } else if (boardRep[i][newCoord].color === whichColor()) {
+            tempUpRightMoves.push([i, coord[1] + num]);
+            break;
+          }
+          tempUpRightMoves.push([i, coord[1] + num]);
+        }
       }
       tempMoves.push(...tempUpRightMoves);
     };
     const generateDownRight = () => {
       const tempUpDownMoves = [];
       let num = 0;
-      for (let i = coord[0]; i < 8; i += 1) {
-        tempUpDownMoves.push([i, coord[1] + num]);
+      for (let i = coord[0] + 1; i < 8; i += 1) {
         num += 1;
+        const newCoord = coord[1] + num;
+        if (newCoord < 8 && newCoord >= 0) {
+          if (boardRep[i][newCoord].color === color) {
+            break;
+          } else if (boardRep[i][newCoord].color === whichColor()) {
+            tempUpDownMoves.push([i, coord[1] + num]);
+            break;
+          }
+          tempUpDownMoves.push([i, coord[1] + num]);
+        }
       }
       tempMoves.push(...tempUpDownMoves);
     };
     const generateDownLeft = () => {
       const tempDownLefttMoves = [];
       let num = 0;
-      for (let i = coord[0]; i < 8; i += 1) {
-        tempDownLefttMoves.push([i, coord[1] - num]);
+      for (let i = coord[0] + 1; i < 8; i += 1) {
         num += 1;
+        const newCoord = coord[1] - num;
+        if (newCoord < 8 && newCoord >= 0) {
+          if (boardRep[i][newCoord].color === color) {
+            break;
+          } else if (boardRep[i][newCoord].color === whichColor()) {
+            tempDownLefttMoves.push([i, coord[1] - num]);
+            break;
+          }
+          tempDownLefttMoves.push([i, coord[1] - num]);
+        }
       }
       tempMoves.push(...tempDownLefttMoves);
     };
     const generateUpLeft = () => {
       const tempUpLeftMoves = [];
       let num = 0;
-      for (let i = coord[0]; i >= 0; i -= 1) {
-        tempUpLeftMoves.push([i, coord[1] - num]);
+      for (let i = coord[0] - 1; i >= 0; i -= 1) {
         num += 1;
+        const newCoord = coord[1] - num;
+        if (newCoord < 8 && newCoord >= 0) {
+          if (boardRep[i][newCoord].color === color) {
+            break;
+          } else if (boardRep[i][newCoord].color === whichColor()) {
+            tempUpLeftMoves.push([i, coord[1] - num]);
+            break;
+          }
+          tempUpLeftMoves.push([i, coord[1] - num]);
+        }
       }
-
       tempMoves.push(...tempUpLeftMoves);
     };
     const generateUp = () => {
       const tempUpMoves = [];
-      for (let i = coord[0]; i >= 0; i -= 1) {
+      for (let i = coord[0] - 1; i >= 0; i -= 1) {
+        if (boardRep[i][coord[1]].color === color) {
+          break;
+        } else if (boardRep[i][coord[1]].color === whichColor()) {
+          tempUpMoves.push([i, coord[1]]);
+          break;
+        }
         tempUpMoves.push([i, coord[1]]);
       }
       tempMoves.push(...tempUpMoves);
     };
     const generateDown = () => {
-      const tempUpMoves = [];
-      for (let i = coord[0]; i < 8; i += 1) {
-        tempUpMoves.push([i, coord[1]]);
+      const tempDownMoves = [];
+      for (let i = coord[0] + 1; i < 8; i += 1) {
+        if (boardRep[i][coord[1]].color === color) {
+          break;
+        } else if (boardRep[i][coord[1]].color === whichColor()) {
+          tempDownMoves.push([i, coord[1]]);
+          break;
+        }
+        tempDownMoves.push([i, coord[1]]);
       }
-      tempMoves.push(...tempUpMoves);
+      tempMoves.push(...tempDownMoves);
     };
     const generateRight = () => {
       const tempRightMoves = [];
-      for (let i = coord[1]; i < 8; i += 1) {
+      for (let i = coord[1] + 1; i < 8; i += 1) {
+        if (boardRep[coord[0]][i].color === color) {
+          break;
+        } else if (boardRep[coord[0]][i].color === whichColor()) {
+          tempRightMoves.push([coord[0], i]);
+          break;
+        }
         tempRightMoves.push([coord[0], i]);
       }
       tempMoves.push(...tempRightMoves);
     };
     const generateLeft = () => {
       const tempLeftMoves = [];
-      for (let i = coord[1]; i >= 0; i -= 1) {
+      for (let i = coord[1] - 1; i >= 0; i -= 1) {
+        if (boardRep[coord[0]][i].color === color) {
+          break;
+        } else if (boardRep[coord[0]][i].color === whichColor()) {
+          tempLeftMoves.push([coord[0], i]);
+          break;
+        }
         tempLeftMoves.push([coord[0], i]);
       }
       tempMoves.push(...tempLeftMoves);
@@ -341,7 +542,10 @@ export const Game = () => {
     generateUpLeft();
 
     setLegalMoves(tempMoves);
-    setActiveLegalMoves(tempMoves, color);
+    const filteredCoord = tempMoves.filter(
+      (move) => coord.toString() !== move.toString(),
+    );
+    setActiveLegalMoves(filteredCoord, color);
   };
 
   const getLegalMoves = (coord, type, color, moved) => {
